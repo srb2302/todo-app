68c8bb77729de3f4c16edb0f8eea0a9097151f3b
srb2302 <105365891+srb2302@users.noreply.github.com>
2026-05-27 04:05:48 +0530

    fixed render deployment

--- run.py
+++ run.py
@@ -5,5 +5,10 @@ app = create_app()
 with app.app_context():
     db.create_all()
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
